881272799369bad19e4f2532fd03fe086e113f24
unknown <mgeden25@gmail.com>
2023-12-19 04:14:44 -0500

    On branch master  Changes to be committed: 	modified:   Main.py Modified the user input for the question would you like to add another number. Also fixed the way it computes the results based on the yes/no anwser. If yes is anwsered it prompts for another number if no is the anwser it only uses the first & second number the progeam does no add a third number.

--- Main.py
+++ Main.py
@@ -1,47 +1,73 @@
-# Simple Calculator
-
 # Function to perform addition
-def add(x, y):
-    return x + y
+def add(x, y, z=None):
+    if z is not None:
+        return x + y + z
+    else:
+        return x + y
 
 # Function to perform subtraction
-def subtract(x, y):
-    return x - y
+def subtract(x, y, z=None):
+    if z is not None:
+        return x - y - z
+    else:
+        return x - y
 
 # Function to perform multiplication
-def multiply(x, y):
-    return x * y
+def multiply(x, y, z=None):
+    if z is not None:
+        return x * y * z
+    else:
+        return x * y
 
 # Function to perform division
-def divide(x, y):
-    if y != 0:
-        return x / y
-    else:
+def divide(x, y, z=None):
+    if z is not None and z != 0:
+        return x / y / z
+    elif z == 0:
         return "Error: Division by zero"
+    else:
+        return x / y
 
-# Take user input
+# Take  in the first , second, & the operation, then asks if the user would like to add another number.
+# The anwsers yes/no are not case sensitive.
 num1 = float(input("Enter the first number: "))
 operation = input("Enter the operation (+, -, *, /): ")
 num2 = float(input("Enter the second number: "))
-AddAnothernum = (input ("Would you Like to add another number yes or no?"))
+AddAnothernum = input("Would you like to add another number? (yes or no): ")
 
-if (isNaN(inputValue1) || isNaN(inputValue2) || isNaN(inputValue3)) {
-        alert("Please enter valid numbers for all inputs.");
-        return;
-      }
+# Converts input to lowercase for case-insensitivity
+AddAnothernum = AddAnothernum.lower()
 
-# Perform calculation based on the chosen operation
-if operation == '+':
-    result = add(num1, num2)
-elif operation == '-':
-    result = subtract(num1, num2)
-elif operation == '*':
-    result = multiply(num1, num2)
-elif operation == '/':
-    result = divide(num1, num2)
+# Perform calculation based on the chosen operation and the user's decision to add another number or not.
+if AddAnothernum == "yes":
+    num3 = float(input("Enter the third number: "))
+    if any(map(lambda x: x.isalpha(), [num1, num2, num3])):
+        result = "Please enter valid numbers only for all inputs."
+    else:
+        if operation == '+':
+            result = add(num1, num2, num3)
+        elif operation == '-':
+            result = subtract(num1, num2, num3)
+        elif operation == '*':
+            result = multiply(num1, num2, num3)
+        elif operation == '/':
+            result = divide(num1, num2, num3)
+        else:
+            result = "Error: Invalid operation"
 else:
-    result = "Error: Invalid operation"
+    if any(map(lambda x: x.isalpha(), [num1, num2])):
+        result = "Please enter valid numbers only for all inputs."
+    else:
+        if operation == '+':
+            result = add(num1, num2)
+        elif operation == '-':
+            result = subtract(num1, num2)
+        elif operation == '*':
+            result = multiply(num1, num2)
+        elif operation == '/':
+            result = divide(num1, num2)
+        else:
+            result = "Error: Invalid operation"
 
 # Display the result
 print(f"Result: {result}")
-
